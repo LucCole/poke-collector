68e875322f6bf4641ac269e9732c08e2858f42be
Luc cole <luccoleb@yahoo.com>
2022-06-03 18:07:13 -0400

    Get all sets

--- api/sets.js
+++ api/sets.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const setsRouter = express.Router();
 
+// Remove when DB added
+const fs = require('fs');
+
+// GET - api/sets/ping
 setsRouter.get('/ping', async (req, res, next) => {
   try{  
     res.status(200).send({success: true});
@@ -10,4 +14,16 @@ setsRouter.get('/ping', async (req, res, next) => {
   }
 });
 
-module.exports = setsRouter;
+// GET - api/sets
+setsRouter.get('/', async (req, res, next) => {
+  try{  
+
+    const {sets} = require('../data/sets.json');
+    res.status(200).send(sets);
+
+  }catch(error){
+    next(error);
+  }
+});
+
+module.exports = setsRouter
--- app.test.js
+++ app.test.js
@@ -63,18 +63,27 @@ describe('Sets', () => {
     });
 
   });
-
   
+  // Get all sets
+  describe('Get all sets', () => {
 
+    let data;
+    let status
 
+    beforeAll(async() => {
+      const responce = await axios.get(`${API_URL}/sets`);
+      data = responce.data;
+      status = responce.status;
+    });
 
+    it("Status is 200", async () => {
+      expect(status).toEqual(200);
+    });
 
+    it("Data is array", async () => {
+      expect(Array.isArray(data)).toEqual(true);
+    });
 
+  });
 
-
-
-
-
-
-  
 });
