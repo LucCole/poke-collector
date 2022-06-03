b20d96c7c27916aa1a464bd5f13e819ffd85ee0f
Luc cole <luccoleb@yahoo.com>
2022-06-03 17:16:13 -0400

    jest

---
+++ app.test.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+const API_URL = 'http://localhost:3001/api';
+
+// Test
+describe('Test', () => {
+
+  it("Returns true", async () => {
+    expect(true).toEqual(true);
+  });
+
+});
