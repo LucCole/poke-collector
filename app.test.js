f28890ab83b5c7d7c96e709241106396317ceb3f
Luc cole <luccoleb@yahoo.com>
2022-06-03 17:33:16 -0400

    API test

--- app.test.js
+++ app.test.js
@@ -1,13 +1,32 @@
 const axios = require('axios');
 const API_URL = 'http://localhost:3001/api';
 
-
-
 // API
-describe('', () => {
+describe('API', () => {
 
-  it("Returns true", async () => {
-    expect(true).toEqual(true);
+  describe('Ping', () => {
+
+    let data;
+    let status
+
+    beforeAll(async() => {
+      const responce = await axios.get(`${API_URL}/sets/ping`);
+      data = responce.data;
+      status = responce.status;
+    });
+
+    it("Status is 200", async () => {
+      expect(status).toEqual(200);
+    });
+
+    it("Data is object", async () => {
+      expect(typeof data).toEqual('object');
+    });
+
+    it("success property equal true", async () => {
+      expect('success' in data && data.success === true).toEqual(true);
+    });
+    
   });
 
 });
